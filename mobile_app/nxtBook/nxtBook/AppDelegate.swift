a351e716983f7b5f6a156d972a26b7bba9b4dc96
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-09-29 12:54:57 -0700

    Added Base Index Controller

--- mobile_app/nxtBook/nxtBook/AppDelegate.swift
+++ mobile_app/nxtBook/nxtBook/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UIApplication.shared.isStatusBarHidden = false
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let controller = IndexController()
+        
+        window?.rootViewController = UINavigationController(rootViewController: controller)
+        
+        
         return true
     }
 
---
+++ mobile_app/nxtBook/nxtBook/Controllers/IndexController.swift
@@ -0,0 +1,17 @@
+//
+//  IndexController.swift
+//  nxtBook
+//
+//  Created by Aatif Jiwani on 9/29/18.
+//  Copyright © 2018 Aatif Jiwani. All rights reserved.
+//
+
+import UIKit
+
+class IndexController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+        navigationController?.navigationBar.isHidden = true
+    }
+}
