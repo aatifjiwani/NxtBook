b40ec3fb20b4d4daa94d9c30fb0f18ca8509201c
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-10-29 20:43:32 -0700

    Added Alert Controller

--- mobile_app/nxtBook/nxtBook/Controllers/EditProfileController.swift
+++ mobile_app/nxtBook/nxtBook/Controllers/EditProfileController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EditProfileController: UIViewController {
+class EditProfileController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
@@ -100,8 +100,28 @@ class EditProfileController: UIViewController {
         return label
     }()
     
+    let alertController = UIAlertController(title: "Choose method", message: "How would you like to upload your picture?", preferredStyle: .actionSheet)
+    
     func setupViews() {
         setupBackground()
+        
+        let sendButton = UIAlertAction(title: "Choose from Photo Library", style: .default, handler: { (action) -> Void in
+            self.changeWithLibrary()
+            print("Library button tapped")
+        })
+        
+        let  deleteButton = UIAlertAction(title: "Choose from Camera", style: .default, handler: { (action) -> Void in
+            self.changeWithCamera()
+            print("Camera button tapped")
+        })
+        
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) -> Void in
+            print("Cancel button tapped")
+        })
+        
+        alertController.addAction(sendButton)
+        alertController.addAction(deleteButton)
+        alertController.addAction(cancelButton)
         
         view.addSubview(backButton)
         backButton.anchor(view.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, topConstant: 30, leftConstant: 20, bottomConstant: 0, rightConstant: 0, widthConstant: 30, heightConstant: 30)
@@ -115,6 +135,10 @@ class EditProfileController: UIViewController {
         view.addSubview(profileImageView)
         profileImageView.anchor(editProfileLabel.bottomAnchor, left: nil, bottom: nil, right: nil, topConstant: 50, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 126, heightConstant: 126)
         profileImageView.anchorCenterXToSuperview()
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(handleChangePicture))
+        profileImageView.isUserInteractionEnabled = true
+        profileImageView.addGestureRecognizer(gesture)
         
         view.addSubview(firstname)
         firstname.anchor(profileImageView.bottomAnchor, left: nil, bottom: nil, right: nil, topConstant: 15, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
@@ -142,6 +166,61 @@ class EditProfileController: UIViewController {
         venmo.anchorCenterSuperview()
         venmo.sizeToFit()
         
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        var selectedImage: UIImage?
+        
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            print(editedImage.size)
+            selectedImage = editedImage
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            print(originalImage.size)
+            selectedImage = originalImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func changeWithLibrary() {
+        print("going...")
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: {
+            print("finished")
+        })
+    }
+    
+    func changeWithCamera() {
+        print("going...")
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: {
+            print("finished")
+        })
+    }
+    
+    @objc func handleChangePicture() {
+        print("p")
+        present(alertController, animated: true, completion: {
+            return
+        })
+//        print("going...")
+//        let imagePicker = UIImagePickerController()
+//        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+//        imagePicker.delegate = self
+//        imagePicker.allowsEditing = true
+//        present(imagePicker, animated: true, completion: {
+//            print("finished")
+//        })
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
     
     @objc func handleBack() {
